3bfc3275fc70ba0a053cb1e2630f627db0b7fd03
Brandon Van Noy <bvannoy@me.com>
2017-07-21 13:05:23 -0500

    feat(build): add build:dist script with versioning and tagging

---
+++ bin/version.js
@@ -0,0 +1,41 @@
+const i = require('inquirer');
+const packageJSON = require('../package.json');
+const semver = require('semver');
+const { spawn } = require('child_process');
+
+console.log(`Current version: ${packageJSON.version}`)
+
+const versionList = [
+  semver.inc(packageJSON.version, 'patch'),
+  semver.inc(packageJSON.version, 'minor'),
+  semver.inc(packageJSON.version, 'major'),
+  'Other'
+]
+
+i.prompt({ name: 'version', message: 'Choose a version:', type: 'list', choices: versionList }).then(answer => {
+  const { version } = answer;
+  if (version === 'Other') return getVersionInput();
+  writeVersion(version);
+});
+
+function getVersionInput() {
+  i.prompt([{ name: 'version', message: 'New version:' }]).then(answer => {
+    const { version } = answer;
+    if (!semver.valid(version)) {
+      console.error(`Supplied version "${version}" is not valid semver`);
+      return getVersionInput();
+    }
+    if (!semver.gt(version, packageJSON.version)) {
+      console.error(`Version "${version}" must be greater than the current version "${packageJSON.version}"`);
+      return getVersionInput();
+    }
+    writeVersion(version);
+  });
+}
+
+function writeVersion(version) {
+  spawn('node_modules/.bin/json', ['-I', '-f', 'package.json', '-e', `this.version="${version}"`]);
+  spawn('node_modules/.bin/json', ['-I', '-f', 'package-lock.json', '-e', `this.version="${version}"`]);
+  spawn('node_modules/.bin/json', ['-I', '-f', 'package.dist.json', '-e', `this.version="${version}"`]);
+  spawn('git', ['tag', `v${version}`]);
+}
