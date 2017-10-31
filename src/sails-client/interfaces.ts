52c1340c943d4372b67a64c2a1a9a7f744c874e3
Brandon Van Noy <bvannoy@me.com>
2017-10-31 18:37:19 -0500

    style: add semicolon

--- src/sails-client/interfaces.ts
+++ src/sails-client/interfaces.ts
@@ -1,28 +1,28 @@
 import { RequestMethod } from './enums';
 
 export interface ISailsRequest {
-  url: string
-  method: RequestMethod
-  headers?: any
-  data?: any
-  params?: any
+  url: string;
+  method: RequestMethod;
+  headers?: any;
+  data?: any;
+  params?: any;
 }
 
 export interface ISailsRequestOpts {
-  headers?: any
-  params?: any
-  search?: any
+  headers?: any;
+  params?: any;
+  search?: any;
 }
 
 export interface IRawSailsResponse {
-  body: any
-  statusCode: number
-  headers: any
+  body: any;
+  statusCode: number;
+  headers: any;
 }
 
 export interface ISailsResponse {
-  data: any
-  status: number
-  headers: any
-  config: ISailsRequest
+  data: any;
+  status: number;
+  headers: any;
+  config: ISailsRequest;
 }
