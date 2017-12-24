03ccae5b3f5038379c05458706d17541c98e499a
Brandon Van Noy <bvannoy@me.com>
2017-12-24 14:41:38 -0600

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,28 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolveAliases from 'rollup-plugin-resolve-aliases';
 import rxPaths from 'rxjs/_esm5/path-mapping';
+import { rollupGlobals } from './tools/package/rollup-globals.ts';
 
 export default {
   input: 'dist/index.js',
+  context: 'this',
   output: {
+    name: 'ng.SailsClient',
+    globals: rollupGlobals,
     file: 'dist/bundles/ngx-sails.umd.js',
-    format: 'umd'
+    format: 'umd',
   },
   sourceMap: false,
-  name: 'ng.SailsClient',
-  onwarn: (warning) => {
-    if (warning.code === 'THIS_IS_UNDEFINED') {
-      return;
-    }
-    console.error(warning.message);
-  },
   plugins: [
     commonjs(),
     resolveAliases({
       aliases: {
         'socket.io-client': 'node_modules/socket.io-client/dist/socket.io.js',
-      }
-    })
+      },
+    }),
   ],
-  external: (id) => {
-    return id.startsWith('@angular') || id.startsWith('rxjs');
-  },
+  external: Object.keys(rollupGlobals),
   resolve: {
-    alias: rxPaths()
+    alias: rxPaths(),
   },
-  globals: {
-    '@angular/core': 'ng.core',
-    '@angular/common': 'ng.common',
-    'rxjs/Observable': 'Rx',
-    'rxjs/Observer': 'Rx',
-    'rxjs/Subject': 'Rx',
-    'rxjs/observable/EmptyObservable': 'Rx.Observable',
-    'rxjs/operators/catchError': 'Rx.Observable',
-    'rxjs/operators/map': 'Rx.Observable',
-  }
-}
+};
