b556774f083807d5ce760eb350b16e03d9d2c84d
Brandon Van Noy <bvannoy@me.com>
2017-12-24 18:13:53 -0600

    feat(build): add tslint rules, add tslint to build scripts
    * move version helper to new tools dir
    * switch from npm-watch to watch
    * refactor and update rollup config

---
+++ tools/package/rollup-globals.ts
@@ -0,0 +1,16 @@
+export const rollupGlobals = {
+  '@angular/core': 'ng.core',
+  '@angular/common': 'ng.common',
+  '@angular/core/testing': 'ng.core.testing',
+
+  'socket.io-client': 'node_modules/socket.io-client/dist/socket.io.js',
+
+  'rxjs/Observable': 'Rx',
+  'rxjs/Observer': 'Rx',
+  'rxjs/Subject': 'Rx',
+
+  'rxjs/observable/EmptyObservable': 'Rx.Observable',
+
+  'rxjs/operators/catchError': 'Rx.operators',
+  'rxjs/operators/map': 'Rx.operators',
+};
